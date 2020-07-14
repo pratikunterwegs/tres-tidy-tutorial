76e84920e9a0f97e477c416f0319ecd91270cb30
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-14 07:57:01 +0200

    Check for style, progress #49

---
+++ check_style.R
@@ -0,0 +1,7 @@
+rmd_filenames <- sort(list.files(pattern = "*.Rmd"))
+for (rmd_filename in rmd_filenames) {
+  message(paste0(rep("*", times = 80), collapse = ""))
+  message(rmd_filename)
+  message(paste0(rep("*", times = 80), collapse = ""))
+  print(lintr::lint(rmd_filename), sep = "\n")
+}
